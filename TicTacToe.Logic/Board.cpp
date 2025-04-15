8314c4781103b80ee93405fea7869c00c8cf13d9
Michael Lauritzen <michael.lauritzen@students.snow.edu>
2025-04-15 08:07:26 -0600

    i tested test square and board create

--- TicTacToe.Logic/Board.cpp
+++ TicTacToe.Logic/Board.cpp
@@ -4,16 +4,31 @@
 
 void Board::SetBoard(char setup[3][3])
 {
-	
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			grid[i][j] = setup[i][j];
+		}
+	}
 }
 
 char Board::GetSquare(int row, int col) const
 {
-	return 'X';
+	return grid[row][col];
 }
 
 bool Board::SetSquare(int row, int col, char player)
 {
+	if (grid[row][col] == ' ')
+	{
+		if (player == 'X')
+			grid[row][col] = 'X';
+
+		else
+			grid[row][col] = 'O';
+		return true;
+	}
 	return false;
 }
 
