4dba40832337d5422a8ac3c2305edf7462997c32
Michael Lauritzen <michael.lauritzen@students.snow.edu>
2025-04-15 08:31:00 -0600

    added untested win conditions

--- TicTacToe.Logic/Board.cpp
+++ TicTacToe.Logic/Board.cpp
@@ -34,6 +34,24 @@ bool Board::SetSquare(int row, int col, char player)
 
 char Board::GameStatus()
 {
+	if (grid[0][0] == grid[0][1] && grid[0][1] == grid[0][2] && grid[0][0] != ' ')
+		return grid[0][0];
+	if (grid[1][0] == grid[1][1] && grid[1][1] == grid[1][2] && grid[1][0] != ' ')
+		return grid[1][0];
+	if (grid[2][0] == grid[2][1] && grid[2][1] == grid[2][2] && grid[2][0] != ' ')
+		return grid[2][0];
+
+	if (grid[0][0] == grid[1][0] && grid[1][0] == grid[2][0] && grid[0][0] != ' ')
+		return grid[0][0];
+	if (grid[0][1] == grid[1][1] && grid[1][1] == grid[2][1] && grid[0][1] != ' ')
+		return grid[0][1];
+	if (grid[0][2] == grid[1][2] && grid[1][2] == grid[2][2] && grid[0][2] != ' ')
+		return grid[0][2];
 	return ' ';
+
+	if (grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2] && grid[0][0] != ' ')
+		return grid[0][0];
+	if (grid[0][2] == grid[1][1] && grid[1][1] == grid[2][0] && grid[0][2] != ' ')
+		return grid[0][0];
 }
 
